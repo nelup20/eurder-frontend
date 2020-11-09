e09534037f31d879077a3c348815af72e94bfe0b
Nelu Platonov <platon.nelu@yahoo.com>
2020-11-09 16:16:00 +0100

    Story 3 done. Will refactor after this since it's starting to look like a mess

--- src/App.js
+++ src/App.js
@@ -31,7 +31,26 @@ class App extends React.Component{
       },
       returnedResult: "",
       requestWasSuccessful: true
-    }
+    },
+
+    story3: {
+      inputs: {
+        numberOfItems: 1,
+        customerId: "c6093628-b11a-4ece-b2f0-509fc0f3c132",
+        items: [
+          {
+            itemId: "44492ce0-dfca-49f5-b519-0bf2839f2d64",
+            itemQuantityToOrder: 5
+        },
+        {
+          itemId: "bc23cbd0-fc7a-404d-a473-39711a0f7c7c",
+          itemQuantityToOrder: 22
+        }
+      ]
+      },
+      returnedResult: "",
+      requestWasSuccessful: true
+    },
   }
 
   highlightInputIfEmpty = (story, inputName) => {
@@ -91,6 +110,17 @@ class App extends React.Component{
     this.makePostRequestAndDisplayResult("/items", body, headers, "story2");
   }
 
+  OrderItems = () => {
+    let headers = {
+      "Content-Type": "application/json",
+      "customerId": this.state.story3.inputs.customerId
+    };
+
+    let body = JSON.stringify(this.state.story3.inputs.items);
+
+    this.makePostRequestAndDisplayResult("/orders", body, headers, "story3");
+  }
+
   makePostRequestAndDisplayResult = (endpoint, body, headers, story) => {
     if(this.areAllInputsInStoryNotEmpty(story)){
 
@@ -130,7 +160,40 @@ class App extends React.Component{
     this.setState(oldState);
   }
 
+  handleStory3Inputs = (event) => {
+    let itemIndex = event.target.getAttribute("data-item-index");
+    let inputName = event.target.name; 
+    let inputValue = event.target.value;
+
+    let oldState = this.state;
+
+    oldState.story3.inputs.items[itemIndex][inputName] = inputValue;
+
+    this.setState(oldState);
+  }
+
+  addItemToOrder = () => {
+    let oldState = this.state;
+    oldState.story3.inputs.items.push({
+      itemId: "",
+      itemQuantityToOrder: 0
+    });
+
+    this.setState(oldState);
+  }
+
+  removeItemToOrder = () => {
+    let oldState = this.state;
+    oldState.story3.inputs.items.pop();
+
+    this.setState(oldState);
+  }
+
+
+
+
   render(){
+    
     return(
       <div className="container">
         <img src="order-logo.png" id="order-logo"/>
@@ -231,7 +294,7 @@ class App extends React.Component{
         </Section>
 
         <Section>
-        <Heading>#2 - Add an item: POST request to /items</Heading>
+        <Heading>Story #2 - Add an item: POST request to /items</Heading>
         <div className="endpointSectionBody">
           <div className="endpointSectionBody-inputSide">
             
@@ -301,6 +364,80 @@ class App extends React.Component{
         <div className={"endpointSectionResult" + (this.state.story2.returnedResult.length === 0 ? " is-hidden" : "")}>
           <Heading size={4} >Results:</Heading>
           <textarea className={"textarea " + (this.state.story2.requestWasSuccessful ? " is-success" : " is-danger")} rows={7} value={this.state.story2.returnedResult} readOnly></textarea>
+        </div>
+        </Section>
+
+
+        <Section>
+        <Heading>Story #3 - Order items: POST request to /orders</Heading>
+        <div className="endpointSectionBody">
+          <div className="endpointSectionBody-inputSide">
+            
+            <div className="field">
+              <label className="label">Customer ID</label>
+              <div className="control">
+                <input type="text" className={ "input" + this.highlightInputIfEmpty("story3", "customerId") } value={this.state.story3.inputs.customerId} onChange={this.handleInputChange} data-story="story3" name="customerId" placeholder="ab9s3i16-53ca-4e5e-85ef-4ed3dc59f356"/>
+                <p className={"help "  + this.displayHelperMessageIfInputEmpty("story3", "customerId")}>Customer ID can't be empty</p>
+              </div>
+            </div>
+
+            <Heading size={5} id="itemsToOrderHeading">Items to order:</Heading>
+            <div className="itemsToOrder">
+              {this.state.story3.inputs.items.map((value, index) => {
+                return (
+                  <div key={index}>
+                          <div className="field">
+                            <label className="label">Item #{index+1}'s ID</label>
+                              <div className="control">
+                                <input type="text" className={ "input"} value={value.itemId} onChange={this.handleStory3Inputs} data-story="story3" data-item-index={index} name="itemId" placeholder="ab9s3i16-53ca-4e5e-85ef-4ed3dc59f356"/>
+                                {/* <p className={"help "  + this.displayHelperMessageIfInputEmpty("story3", "name")}>Item name can't be empty</p> */}
+                              </div>
+                            </div>
+
+                            <div className="field">
+                              <label className="label">Amount of item #{index+1} to order</label>
+                              <div className="control">
+                                <input type="number" min={0} className={"input " } value={value.itemQuantityToOrder} onChange={this.handleStory3Inputs} data-item-index={index} data-story="story3" name="itemQuantityToOrder" placeholder="5"/>
+                                {/* <p className={"help " + this.displayHelperMessageIfInputEmpty("story2", "description")}>Item's description can't be empty</p> */}
+                              </div>
+                            </div>
+                            <br></br>
+                    </div>
+                )
+              })}
+            </div>
+
+          
+            <div className="field is-grouped">
+            <div className="control">
+                <button className="button is-link" onClick={this.addItemToOrder}>Add Items to order</button>
+              </div>
+              <div className="control">
+                <button className="button is-danger" onClick={this.removeItemToOrder}>Remove Items to order</button>
+              </div>
+              <div className="control">
+                <button className="button is-primary" onClick={this.OrderItems}>Order Items</button>
+              </div>
+            </div>
+
+          </div>
+          <Hero className="has-background-grey-lighter endpointSectionBody-descriptionSide" id="initialDataHero">
+            <Hero.Body className="is-size-5">
+              <div><span className="has-text-weight-bold">Requirements:</span> A Customer ID in the Header for the customerId key. Also a JSON payload in the body which contains an Array of objects with the following fields:
+              <ul>
+                <li>itemId: String</li>
+                <li>itemQuantityToOrder: Integer</li>
+              </ul>
+              </div>
+              <br/>
+              <div><span className="has-text-weight-bold">Expected Response:</span> JSON object of the newly created order</div>
+            </Hero.Body>
+          </Hero>
+          
+        </div>
+        <div className={"endpointSectionResult" + (this.state.story3.returnedResult.length === 0 ? " is-hidden" : "")}>
+          <Heading size={4} >Results:</Heading>
+          <textarea className={"textarea " + (this.state.story3.requestWasSuccessful ? " is-success" : " is-danger")} rows={7} value={this.state.story3.returnedResult} readOnly></textarea>
         </div>
         </Section>
       </div>
